b1936893c26400a433b243ad47786589883aa731
Vlad <vlad-ard@ya.ru>
2021-11-14 14:24:04 +0300

    Init Buttons

--- app/src/main/java/com/homework/calculator/MainActivity.java
+++ app/src/main/java/com/homework/calculator/MainActivity.java
@@ -3,16 +3,42 @@ package com.homework.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        textView = findViewById(R.id.textView);
+
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+
+        Button buttonComma = findViewById(R.id.buttonComma);
+        Button buttonAC = findViewById(R.id.buttonAC);
+        Button buttonNegative = findViewById(R.id.buttonNegative);
+        Button buttonPercent = findViewById(R.id.buttonPercent);
+        Button buttonDivide = findViewById(R.id.buttonDevide);
+        Button buttonMultiply = findViewById(R.id.buttonMultiply);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+        Button buttonEqual = findViewById(R.id.buttonEqual);
+
+
+
     }
 }
