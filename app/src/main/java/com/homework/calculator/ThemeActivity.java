664dd56abc7cb0981cb98ef2a2b9c686b1c2cb1a
Vlad <vlad-ard@ya.ru>
2021-11-21 18:49:34 +0300

    Сделал новую ActivitySettings Сделал для неё макет

---
+++ app/src/main/java/com/homework/calculator/ThemeActivity.java
@@ -0,0 +1,33 @@
+package com.homework.calculator;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThemeActivity extends AppCompatActivity {
+    private static final String Login = "AppTheme2";
+    private static final String NameSharedPreference = "setup";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(getAppTheme());
+    }
+
+    protected int getAppTheme(){
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        return sharedPref.getInt(Login, R.style.AppThemeLight);
+    }
+
+    // Сохранение настроек
+    protected void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(Login, codeStyle);
+        editor.apply();
+    }
+}
+
+
